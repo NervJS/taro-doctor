324aa914eee8d5cf46501e764cc60de5e83fb4b2
luckyadam <weitaozsh@gmail.com>
2023-05-18 10:18:57 +0800

    feat: 完善项目配置验证

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,25 @@ use std::{fs, error::Error, path::PathBuf, env};
 
 use validators::{ config::ConfigValidator, common::Validator };
 
+use crate::validators::message::{Message, self};
+
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
-pub fn validate_config(config_str: String) -> String {
+pub fn validate_config(config_str: String) {
   let result = validate_config_core(config_str);
   if let Err(e) = result {
-    println!("error, {}", e);
+    println!("{}", Message { kind: message::MessageKind::Error, content: e.to_string() });
   }
-  "Success".to_string()
 }
 
 fn validate_config_core(config_str: String) -> Result<(), Box<dyn Error>> {
+  let tip = Message {
+    kind: validators::message::MessageKind::Info,
+    content: String::from("开始进行项目配置验证！")
+  };
+  println!("{}", tip);
   let current_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
   let mut path = PathBuf::new();
   path.push(current_dir);
@@ -26,13 +32,23 @@ fn validate_config_core(config_str: String) -> Result<(), Box<dyn Error>> {
   path.push("validators");
   path.push("config_schema.json");
   let schema_path = path.as_path();
-  println!("{}", schema_path.to_str().unwrap());
   let schema_str = fs::read_to_string(schema_path)?;
-  if let Ok(config_validator) = ConfigValidator::build(schema_str, config_str) {
-    let result = config_validator.validate();
-    for message in result {
+  let config_validator_result = ConfigValidator::build(schema_str, config_str);
+  let messages = match config_validator_result {
+    Ok(config_validator) => config_validator.validate(),
+    Err(e) => vec![
+      Message {
+        kind: validators::message::MessageKind::Error,
+        content: e.to_string()
+      }
+    ]
+  };
+  if messages.len() > 0 {
+    for message in messages {
       println!("{}", message);
     }
+  } else {
+    println!("{}", Message { kind: message::MessageKind::Success, content: "项目配置正确！".to_string() });
   }
   Ok(())
 }
